910aded684c3a1bed5fd4d74822a64cc0d219aee
rust-master <muhammadzaryabrafique@gmail.com>
2025-05-13 15:33:37 +0500

    app-todo-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TodoComponentComponent } from "./todo-component/todo-component.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TodoComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
