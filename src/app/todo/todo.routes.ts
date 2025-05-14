b0688979eb04959d01313245ce55b44553a8efad
rust-master <muhammadzaryabrafique@gmail.com>
2025-05-14 17:28:11 +0500

    remove files

--- src/app/todo/todo.routes.ts
+++
@@ -1,7 +0,0 @@
-// src/app/todo/todo.routes.ts
-import { Routes } from '@angular/router';
-import { TodoComponentComponent } from './todo-component/todo-component.component';
-
-export const todoRoutes: Routes = [
-  { path: '', component: TodoComponentComponent }
-];
